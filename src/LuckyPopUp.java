7d7bc186ba6516f394e682a2e21f62fca9c87fbe
Mikhail <aemily@users.noreply.github.com>
2019-01-07 04:40:44 +0300

    Now it searches through ru.stackoverflow, opens the first questions and displays the best answer. Removed share/like garbage from output, fixed the bug with output being shorten while beautifyed

--- src/LuckyPopUp.java
+++ src/LuckyPopUp.java
@@ -1,14 +1,12 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
-import javafx.scene.control.SelectionModel;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import sun.jvm.hotspot.ui.Editor;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,16 +17,19 @@ public class LuckyPopUp extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
-        //setting the layout
-        JPanel popPanel = new JPanel();
+        //creating output data
         String selectedText = e.getData(PlatformDataKeys.EDITOR).getSelectionModel().getSelectedText();
         String textToDisplay = "Nothing to see here :(";
         try {
-            textToDisplay = getWebData(selectedText);
+            textToDisplay = generateSearchURL(selectedText);
+            textToDisplay = getQuestionURL(textToDisplay);
+            textToDisplay = getBestAnswer(textToDisplay);
         } catch (IOException e1) {
             e1.printStackTrace();
         }
         textToDisplay = beautify(textToDisplay);
+        //setting the layout
+        JPanel popPanel = new JPanel();
         JTextArea popLabel = new JTextArea(textToDisplay, 25, 50);
         JScrollPane scrollPane = new JScrollPane(popLabel);
         scrollPane.setSize(new Dimension(500,500));
@@ -44,27 +45,56 @@ public class LuckyPopUp extends AnAction {
 
     }
 
-    private String getWebData(String url) throws IOException {
+    private String getBestAnswer(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         Elements answers = doc.getElementsByAttributeValue("class", "answer accepted-answer");
+        answers = answers.attr("class", "post-text");
+        //answers = answers.attr("class", "answercell post-layout--right");
+        answers = answers.attr("itemprop", "text");
+        Element answer = answers.first();
+        Elements children = answer.children();
+        answer = children.get(0);
+        children = answer.children();
+        answer = children.get(1);
+        children = answer.children();
+        answer = children.get(0);
 
-        return answers.text();
+        return answer.text();
     }
 
     private String beautify(String text){
         int counter = 0;
         int lineLength = 40;
-        for(int i = 0; i < text.length()-1; i++)
+        for(int i = 0; i < text.length(); i++)
         {
             if (text.charAt(i) == '\n'){
                 counter = -1;
             }
             if ( text.charAt(i) == ' ' && counter >= lineLength){
-                text = text.substring(0, i)+"\n"+text.substring(i+1, text.length()-1);
+                text = text.substring(0, i)+"\n"+text.substring(i+1, text.length());
                 counter = -1;
             };
             counter++;
         }
         return text;
+    }
+    private String generateSearchURL(String selectedText){
+        for(int i = 0; i < selectedText.length()-1; i++)
+        {
+            if (selectedText.charAt(i) == ' '){
+                selectedText = selectedText.substring(0, i)+"+"+selectedText.substring(i+1, selectedText.length());
+            };
+            if (selectedText.charAt(i) == '\n'){
+                selectedText = selectedText.substring(0, i)+"+"+selectedText.substring(i+1, selectedText.length());
+            }
+        }
+        String searchURL = "https://ru.stackoverflow.com/search?q="+selectedText;
+        return searchURL;
+    }
+    private String getQuestionURL(String searchURL) throws IOException {
+        Document doc = Jsoup.connect(searchURL).get();
+        Elements h3Tags = doc.select("h3");
+        Element firstAnswer = h3Tags.select("a[href]").get(2);
+        return "https://ru.stackoverflow.com"+firstAnswer.attr("href");
     }
 }
